5be900f1ebde0ce73d0dc2876ad1a3bec26ecd71
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-11-02 20:32:21 +0530

    done

---
+++ lib/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "email", type: "text", placeholder: "email" },
+        password: { label: "password", type: "password", placeholder: "" },
+      },
+      async authorize(credentials: any) {
+        return {
+          id: "user1",
+          name: "Sayan",
+          userId: "asd12",
+          email: "ramdomEmail",
+        };
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    jwt: async ({ user, token }: any) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+    session: ({ session, token, user }: any) => {
+      if (session.user) {
+        session.user.id = token.uid;
+      }
+      return session;
+    },
+  },
+});
